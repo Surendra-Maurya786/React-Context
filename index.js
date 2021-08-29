9f5cda9abd7d1f0c5183654392d135c453c54676
Surendra Maurya <62706702+Surendra-Maurya786@users.noreply.github.com>
2021-08-30 00:36:37 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import Comman from './component/Comman';
+
+ReactDOM.render(
+  
+
+    <Comman />
+    
+ ,
+  document.getElementById('root')
+);
+
+
